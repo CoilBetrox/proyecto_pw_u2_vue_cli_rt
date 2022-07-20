feadea3be54e4eae7bdf463da16057e6336806e2
roberthdanielt <roberthdanielt@gmail.com>
2022-07-20 13:31:50 -0500

    taller16

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <!-- <Contador2 titulo="titulo1" v-bind:inicio="valorInicio"/> -->
-  <!-- <Contador2 titulo="titulo3" v-bind:inicio="6"/> -->
-  <datosBasicos v-bind:nombre="tipo" v-bind:apellido="tipo" v-bind:correo="tipo"/>
+  
+  <Pregunta/>
 
-  <h1 />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Contador from './components/contador.vue'
-import Contador2 from './components/contador2.vue'
-import datosBasicos from './components/datosBasicos.vue'
-
-
+import Pregunta from './components/Pregunta.vue'
 
 export default {
   name: 'App',
   data () {
         return{
-           valorInicio: 26,
-           tipo: 'Estudiante'
+           
         }
   },
   components: {
-    Contador,
-    Contador2,
-    datosBasicos
-    
+    Pregunta
   }
 }
 </script>
